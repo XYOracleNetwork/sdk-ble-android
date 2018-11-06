48d3b8091d379dc88fd60680c824cb497f52aacb
Carter Harrison <carterjharrison@gmail.com>
2018-11-06 10:23:40 -0800

    build sample app

--- sdk-android-sample/src/main/java/network/xyo/ble/sample/fragments/AdvertiserFragment.kt
+++ sdk-android-sample/src/main/java/network/xyo/ble/sample/fragments/AdvertiserFragment.kt
@@ -32,6 +32,7 @@ class AdvertiserFragment(private val advertiser: XYBluetoothAdvertiser) : XYBase
             updatePrimaryServiceData(view)
             updateManufactureId(view)
             updateManufactureData(view)
+            advertiser.startAdvertising()
         }
     }
 
@@ -42,10 +43,10 @@ class AdvertiserFragment(private val advertiser: XYBluetoothAdvertiser) : XYBase
         val idx = radioButtonGroup.indexOfChild(radioButton)
 
         when(idx) {
-            0 -> advertiser.changeAdvertisingMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
-            1 -> advertiser.changeAdvertisingMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
-            2 -> advertiser.changeAdvertisingMode(AdvertiseSettings.ADVERTISE_MODE_LOW_POWER)
-            3 -> advertiser.changeAdvertisingMode(null)
+            0 -> advertiser.changeAdvertisingMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY, false)
+            1 -> advertiser.changeAdvertisingMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED, false)
+            2 -> advertiser.changeAdvertisingMode(AdvertiseSettings.ADVERTISE_MODE_LOW_POWER, false)
+            3 -> advertiser.changeAdvertisingMode(null, false)
         }
     }
 
@@ -56,11 +57,11 @@ class AdvertiserFragment(private val advertiser: XYBluetoothAdvertiser) : XYBase
         val idx = radioButtonGroup.indexOfChild(radioButton)
 
         when(idx) {
-            0 -> advertiser.changeAdvertisingTxLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
-            1 -> advertiser.changeAdvertisingTxLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
-            2 -> advertiser.changeAdvertisingTxLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
-            3 -> advertiser.changeAdvertisingTxLevel(AdvertiseSettings.ADVERTISE_TX_POWER_ULTRA_LOW)
-            4 -> advertiser.changeAdvertisingTxLevel(null)
+            0 -> advertiser.changeAdvertisingTxLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH, false)
+            1 -> advertiser.changeAdvertisingTxLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM, false)
+            2 -> advertiser.changeAdvertisingTxLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH, false)
+            3 -> advertiser.changeAdvertisingTxLevel(AdvertiseSettings.ADVERTISE_TX_POWER_ULTRA_LOW, false)
+            4 -> advertiser.changeAdvertisingTxLevel(null, false)
         }
     }
 
@@ -71,18 +72,18 @@ class AdvertiserFragment(private val advertiser: XYBluetoothAdvertiser) : XYBase
         val idx = radioButtonGroup.indexOfChild(radioButton)
 
         when(idx) {
-            0 -> advertiser.changeContactable(true)
-            1 -> advertiser.changeContactable(false)
-            2 -> advertiser.changeContactable(null)
+            0 -> advertiser.changeContactable(true, false)
+            1 -> advertiser.changeContactable(false, false)
+            2 -> advertiser.changeContactable(null, false)
         }
     }
 
     private fun updateTimeout(view: View) {
         try {
             val timeoutTime = Integer.parseInt(view.timeout_input.text.toString())
-            advertiser.changeTimeout(timeoutTime)
+            advertiser.changeTimeout(timeoutTime, false)
         } catch (e : NumberFormatException) {
-            advertiser.changeTimeout(null)
+            advertiser.changeTimeout(null, false)
         }
     }
 
@@ -93,9 +94,9 @@ class AdvertiserFragment(private val advertiser: XYBluetoothAdvertiser) : XYBase
         val idx = radioButtonGroup.indexOfChild(radioButton)
 
         when(idx) {
-            0 -> advertiser.changeIncludeDeviceName(true)
-            1 -> advertiser.changeIncludeDeviceName(false)
-            2 -> advertiser.changeIncludeDeviceName(null)
+            0 -> advertiser.changeIncludeDeviceName(true, false)
+            1 -> advertiser.changeIncludeDeviceName(false, false)
+            2 -> advertiser.changeIncludeDeviceName(null, false)
         }
     }
 
@@ -106,38 +107,38 @@ class AdvertiserFragment(private val advertiser: XYBluetoothAdvertiser) : XYBase
         val idx = radioButtonGroup.indexOfChild(radioButton)
 
         when(idx) {
-            0 -> advertiser.changeIncludeTxPowerLevel(true)
-            1 -> advertiser.changeIncludeTxPowerLevel(false)
-            2 -> advertiser.changeIncludeTxPowerLevel(null)
+            0 -> advertiser.changeIncludeTxPowerLevel(true, false)
+            1 -> advertiser.changeIncludeTxPowerLevel(false, false)
+            2 -> advertiser.changeIncludeTxPowerLevel(null, false)
         }
     }
 
     private fun updatePrimaryServiceUuid(view: View) {
         try {
             val uuid = ParcelUuid(UUID.fromString(view.primary_service_input.toString()))
-            advertiser.chnagePrimaryService(uuid)
+            advertiser.chnagePrimaryService(uuid, false)
         } catch (e : IllegalArgumentException) {
-            advertiser.chnagePrimaryService(null)
+            advertiser.chnagePrimaryService(null, false)
         }
     }
 
     private fun updatePrimaryServiceData(view: View) {
         val data = view.primary_service_data_input.toString().toByteArray()
-        advertiser.changePrimaryServiceData(data)
+        advertiser.changePrimaryServiceData(data, false)
     }
 
     private fun updateManufactureId(view: View) {
         try {
             val id = Integer.parseInt(view.manufacturer_id_input.text.toString())
-            advertiser.changeManufacturerId(id)
+            advertiser.changeManufacturerId(id, false)
         } catch (e : NumberFormatException) {
-            advertiser.changeManufacturerId(null)
+            advertiser.changeManufacturerId(null, false)
         }
     }
 
     private fun updateManufactureData(view: View) {
         val data = view.manufacturer_data_input.toString().toByteArray()
-        advertiser.changePrimaryServiceData(data)
+        advertiser.changePrimaryServiceData(data, false)
     }
 
     companion object {
