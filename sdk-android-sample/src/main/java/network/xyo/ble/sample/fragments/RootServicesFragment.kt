f8ca778a5e80c5582c2a8d49a74bf0b474b9a12c
Carter Harrison <carterjharrison@gmail.com>
2018-12-18 16:15:23 -0800

    fixed constructor

--- sdk-android-sample/src/main/java/network/xyo/ble/sample/fragments/RootServicesFragment.kt
+++ sdk-android-sample/src/main/java/network/xyo/ble/sample/fragments/RootServicesFragment.kt
@@ -15,7 +15,7 @@ class RootServicesFragment : XYBaseFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_services_root, container, false)
         val transaction = fragmentManager!!.beginTransaction()
-        servicesFragment = ServicesFragment(startingServices)
+        servicesFragment = ServicesFragment.newInstance(startingServices)
         transaction.replace(R.id.root_frame_services, servicesFragment!!)
         transaction.commit()
         return view
