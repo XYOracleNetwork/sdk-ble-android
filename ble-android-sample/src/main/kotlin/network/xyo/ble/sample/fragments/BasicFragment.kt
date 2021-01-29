5ada197b099061e962d42ad40eedc28c11cc4036
Arie Trouw <arie.trouw@xy.company>
2021-01-28 22:48:17 -0800

    more cleanup

--- ble-android-sample/src/main/kotlin/network/xyo/ble/sample/fragments/BasicFragment.kt
+++ ble-android-sample/src/main/kotlin/network/xyo/ble/sample/fragments/BasicFragment.kt
@@ -11,6 +11,7 @@ import network.xyo.ble.devices.xy.XY3BluetoothDevice
 import network.xyo.ble.generic.devices.XYBluetoothDevice
 import network.xyo.ble.sample.XYDeviceData
 import network.xyo.ble.sample.databinding.FragmentBasicBinding
+import network.xyo.ble.sample.ui
 import network.xyo.ble.services.xy.BasicConfigService
 
 @kotlin.ExperimentalUnsignedTypes
@@ -43,31 +44,31 @@ class BasicFragment(device: XYBluetoothDevice, deviceData : XYDeviceData) : XYDe
         basic?.let {
 
             val lock = it.lock.get()
-            activity?.runOnUiThread { binding.textLock.text = lock.toString() }
+            ui { binding.textLock.text = lock.toString() }
 
             val unlock = it.unlock.get()
-            activity?.runOnUiThread { binding.textUnlock.text = unlock.toString() }
+            ui { binding.textUnlock.text = unlock.toString() }
 
             val major = it.major.get()
-            activity?.runOnUiThread { binding.textMajor.text = major.toString() }
+            ui { binding.textMajor.text = major.toString() }
 
             val minor = it.minor.get()
-            activity?.runOnUiThread { binding.textMinor.text = minor.toString() }
+            ui { binding.textMinor.text = minor.toString() }
 
             val uuid = it.uuid.get()
-            activity?.runOnUiThread { binding.textUuid.text = uuid.toString() }
+            ui { binding.textUuid.text = uuid.toString() }
 
             val reboot = it.reboot.get()
-            activity?.runOnUiThread { binding.textReboot.text = reboot.toString() }
+            ui { binding.textReboot.text = reboot.toString() }
 
             val interval = it.interval.get()
-            activity?.runOnUiThread { binding.textInterval.text = interval.toString() }
+            ui { binding.textInterval.text = interval.toString() }
 
             val lockStatus = it.lockStatus.get()
-            activity?.runOnUiThread { binding.textLockStatus.text = lockStatus.toString() }
+            ui { binding.textLockStatus.text = lockStatus.toString() }
 
             val otaWrite = it.otaWrite.get()
-            activity?.runOnUiThread { binding.textOtaWrite.text = otaWrite.toString() }
+            ui { binding.textOtaWrite.text = otaWrite.toString() }
         }
     }
 }
